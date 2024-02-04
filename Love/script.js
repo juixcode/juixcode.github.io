280a1d492d994c425f23cd0ce00d27bd0a39def4
Juju Develop <juixcontact@gmail.com>
2024-02-04 23:51:46 +0100

    bug

--- Love/script.js
+++ Love/script.js
@@ -1,11 +1,12 @@
 const affichage = document.getElementById("resultat");
 const button_heart = document.querySelector('button')
+const audio = new Audio("Medias/done.mp3")
 let resultat = 0;
 
 function add() {
 	resultat += 1;
 	affichage.textContent = (String(resultat)); // AFFICHAGE DU RESULTAT
-	new Audio("Medias/done.mp3").play();
+	audio.play();
 
     affichage.className = "";
     button_heart.className = "";
