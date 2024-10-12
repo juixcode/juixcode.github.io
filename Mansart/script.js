34e04c53155f6bade3efe539b3751eba94963c72
Juju Develop <juixcontact@gmail.com>
2024-10-12 02:01:39 +0200

    up

--- Mansart/script.js
+++ Mansart/script.js
@@ -3,3 +3,10 @@ let juixAd = document.querySelector('.pub');
 function closeAd() {
     juixAd.classList.add('active');
 }
+
+let body = document.querySelector('body');
+
+function switchPage(num) {
+    document.querySelector('.current-page').classList.remove('current-page');
+    body.children[num].classList.add('current-page');
+}
